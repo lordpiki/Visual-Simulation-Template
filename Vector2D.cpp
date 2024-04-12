3fca275d1ede8aeb046de1132bd60a1b8c715e30
Miki Dovlekaev <miki.dovlekaev2510@gmail.com>
2024-04-12 17:41:16 +0300

    Added Vector2D

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -1,1 +1,16 @@
 #include "Vector2D.h"
+
+Vector2D::Vector2D(float x, float y) : x(x), y(y) {}
+
+Vector2D Vector2D::operator+(const Vector2D& other) const {
+    return Vector2D(x + other.x, y + other.y);
+}
+
+Vector2D Vector2D::operator-(const Vector2D& other) const {
+    return Vector2D(x - other.x, y - other.y);
+}
+
+Vector2D Vector2D::operator*(float scalar) const {
+    return Vector2D(x * scalar, y * scalar);
+}
+// ... (Implement other vector operations)
--- Vector2D.h
+++ Vector2D.h
@@ -1,5 +1,13 @@
 #pragma once
-class Vector2D
-{
-};
 
+class Vector2D {
+public:
+    float x, y;
+
+    Vector2D(float x = 0.0f, float y = 0.0f);
+
+    Vector2D operator+(const Vector2D& other) const;
+    Vector2D operator-(const Vector2D& other) const;
+    Vector2D operator*(float scalar) const;
+    // ... (Add other vector operations as needed)
+};
